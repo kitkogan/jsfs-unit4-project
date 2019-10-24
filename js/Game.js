d01628ea84ca2a78c5351540e991e2d4f1220817
Kit Kogan <kitkogan@users.noreply.github.com>
2019-10-24 18:01:20 -0500

    create classes
    game / phrase classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,16 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [ 
+           
+    ]; // game phrases
+        this.activePhrase = null;
+    }
+
+    
+
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+    constructor(phrase) {
+       this.phrase = phrase.toLowerCase(); 
+    } // actual phrase object is representing, set to lower case
+    
+}
