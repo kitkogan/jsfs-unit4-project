588e199d92f4d827cc69fad11f8f81aafacadbdf
Kit Kogan <kitkogan@users.noreply.github.com>
2019-10-25 11:45:12 -0500

    add startGame method and start button click handler

--- js/Game.js
+++ js/Game.js
@@ -13,14 +13,24 @@
             new Phrase('Several spooky slimy spiders spun sulking by the sea')
         ];
         this.activePhrase = null;
-    }
+    } // constructor containing: tracker for num of missed guesses, 
+            //bank of phrases the game will randomly choose from, 
+            //phrase currently in play
 
     getRandomPhrase() {
         const phrases = this.phrases;
         const randomPhrase = Math.floor(Math.random() * 5);
         return phrases[randomPhrase];
-    }
+    } // selects random phrase from phrase property
 
+    startGame() {
+        const overlayDiv =  document.getElementById('overlay');
+        this.activePhrase = this.getRandomPhrase();
+        const currPhrase = this.activePhrase;
+        overlayDiv.style.display = 'none';
+        currPhrase.addPhraseToDisplay;
+        
+    } // selects random phrase and displays it to user on game start
 }                         
 
  
--- js/app.js
+++ js/app.js
@@ -2,20 +2,9 @@
  * Project 4 - OOP Game App
  * app.js */
 
-// const logphrase = (phrase) => {
-//     console.log('Phrase - phrase: ', phrase.phrase);
-// };
-
-
 const game = new Game();
-game.getRandomPhrase().addPhraseToDisplay();
+const startButton = document.getElementById('btn__reset');
 
-//  const game = new Game();
-//  const randomPhrase = game.getRandomPhrase();
-//  const phrase = new Phrase(randomPhrase.phrase);
-//  phrase.addPhraseToDisplay();
-// logphrase(game.getRandomPhrase());
-// logphrase(game.getRandomPhrase());
-// logphrase(game.getRandomPhrase());
-// logphrase(game.getRandomPhrase());
-// logphrase(game.getRandomPhrase());
+startButton.addEventListener('click', function() {
+        game.startGame();
+});
