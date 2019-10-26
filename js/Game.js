a86575d2f7580ef7eb930cf6719339874154e698
Kit Kogan <kitkogan@users.noreply.github.com>
2019-10-25 19:13:53 -0500

    revert to previous draft

--- js/Game.js
+++ js/Game.js
@@ -24,54 +24,54 @@ class Game {
         return phrases[randomPhrase];
     } // selects random phrase from phrase property
 
-    startGame() {
-        const overlayDiv =  document.getElementById('overlay');
-        this.activePhrase = this.getRandomPhrase();
-        const currPhrase = this.activePhrase;
-        overlayDiv.style.display = 'none';
-        currPhrase.addPhraseToDisplay;
-    } // selects random phrase and displays it to user on game start
+    // startGame() {
+    //     const overlayDiv =  document.getElementById('overlay');
+    //     this.activePhrase = this.getRandomPhrase();
+    //     const currPhrase = this.activePhrase;
+    //     overlayDiv.style.display = 'none';
+    //     currPhrase.addPhraseToDisplay;
+    // } // selects random phrase and displays it to user on game start
     
-    checkForWin() {
-        const letters = document.getElementsByClassName('letter');
-        let notGuessed = 0
-        for(let letter of letters) {
-            if(letter.classList.contains('show')) {
-                notGuessed++;
-                return 'try again';
-            }
-        }
-        if(notGuessed === 0) {
-            return 'you win';
-        }
-    } // checks for winning move
+    // checkForWin() {
+    //     const letters = document.getElementsByClassName('letter');
+    //     let notGuessed = 0
+    //     for(let letter of letters) {
+    //         if(letter.classList.contains('show')) {
+    //             notGuessed++;
+    //             return 'try again';
+    //         }
+    //     }
+    //     if(notGuessed === 0) {
+    //         return 'you win';
+    //     }
+    // } // checks for winning move
 
-    removeLife() {
-        this.missed++;
-        const minusOne = document.getElementsByTagName('img')[this.missed - 1];
-        minusOne.setAttribute('src', images/lostHeart.png);
-        if(hearts[4].getAttribute('src') === 'images/lostHeart.png') {
-            this.gameOver('sorry you lose');
-            this.missed = 0;
-        }
-    } // removes a life from scoreboard
+    // removeLife() {
+    //     this.missed++;
+    //     const minusOne = document.getElementsByTagName('img')[this.missed - 1];
+    //     minusOne.setAttribute('src', images/lostHeart.png);
+    //     if(hearts[4].getAttribute('src') === 'images/lostHeart.png') {
+    //         this.gameOver('sorry you lose');
+    //         this.missed = 0;
+    //     }
+    // } // removes a life from scoreboard
 
-    gameOver(gameWon) {
-        const endGame = document.querySelector('#game-over-message');
-        overlayDiv.classList.remove('start');
-        document.getElementById('btn__reset').textContent = 'Try again?'
-        if(gameWon === 'winner') {
-            overlayDiv.classList.remove('lose');
-            overlayDiv.classList.add('win');
-            endGame.textContent = 'You guessed the phrase!'
-        }
-        else {
-            overlayDiv.classlist.remove('win');
-            overlayDiv.classList.add('lose');
-            endGame.textContent = 'Sorry, all out of moves!';
-        }
+    // gameOver(gameWon) {
+    //     const endGame = document.querySelector('#game-over-message');
+    //     overlayDiv.classList.remove('start');
+    //     document.getElementById('btn__reset').textContent = 'Try again?'
+    //     if(gameWon === 'winner') {
+    //         overlayDiv.classList.remove('lose');
+    //         overlayDiv.classList.add('win');
+    //         endGame.textContent = 'You guessed the phrase!'
+    //     }
+    //     else {
+    //         overlayDiv.classlist.remove('win');
+    //         overlayDiv.classList.add('lose');
+    //         endGame.textContent = 'Sorry, all out of moves!';
+    //     }
     
   
-    }
+    // }
 
 }
--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@
  * Project 4 - OOP Game App
  * app.js */
 
-let game = new Game();
-const startButton = document.getElementById('btn__reset');
+// let game = new Game();
+// const startButton = document.getElementById('btn__reset');
 
-startButton.addEventListener('click', function() {
-    game.startGame();   
-}); 
+// startButton.addEventListener('click', function() {
+//     game.startGame();   
+// }); 
+const game = new Game();
+game.getRandomPhrase().addPhraseToDisplay();
