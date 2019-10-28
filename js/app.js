7915c4f341ba33c1b638401a4794d9952c85ab39
Kit Kogan <kitkogan@users.noreply.github.com>
2019-10-27 20:48:27 -0500

    add more comments

--- js/app.js
+++ js/app.js
@@ -8,14 +8,18 @@ const keys = document.getElementsByClassName('key');
 for (let key of keys) {
     key.addEventListener('click', function(event) {
         game.handleInteraction(event);
-    });
+    }); //initiates event handler for on screen keyboard
+            //event triggers on key click
 }
+
 document.getElementById('btn__reset').addEventListener('click', () => {
     game = new Game();
     game.startGame();
-});
+}); // event handler for start game button
+        // event triggers on button click
 
 document.addEventListener('keydown', function(event) {
     game.handleKeyboardInteraction(event);
-});
+}); // event handler for hardware keyboard
+        //event triggers on keydown
 
